f0a26774331a28c97d92bae3c5bddfc729b8f72a
Sumama Rahim <sumama.rahim@yandex.com>
2025-09-26 16:37:24 +0500

    Update Insertion.java

--- DSA/Lab 2 Array/Insertion.java
+++ DSA/Lab 2 Array/Insertion.java
@@ -1,7 +1,6 @@
 // inserting value at the end
 
-
-
+/*
 // brute force approach
 public class Insertion
 {
@@ -12,6 +11,7 @@ public class Insertion
     
     nums[0] = 1; nums[1] = 2; nums[2] = 3; nums[3] = 4;
     
+    
     for (int i = 0; i <= size; i++)
     {
       if (i == 3)
@@ -20,6 +20,27 @@ public class Insertion
       }
     }
     
+    for (int i = 0; i <= size; i++)
+    {
+      System.out.print(nums[i] + " ");
+    }
+  }
+}
+
+**/
+
+// method 2
+public class Insertion
+{
+  public static void main(String... args)
+  {
+    int size = 4, cap = 5;
+    int[] nums = new int[cap];
+    
+    nums[0] = 1; nums[1] = 2; nums[2] = 3; nums[3] = 4;
+    
+    nums[size] = 5;
+
     for (int i = 0; i <= size; i++)
     {
       System.out.print(nums[i] + " ");
